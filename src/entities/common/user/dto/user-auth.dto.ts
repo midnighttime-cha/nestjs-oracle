09cb9c72a8505a4ca8e4af001321edd369cb8af8
Chawalit Sengsang <chawalit1987@gmail.com>
2020-10-04 01:22:19 +0700

    first commit

--- src/entities/common/user/dto/user-auth.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UserAuthDTO {
-  @ApiProperty() @IsNotEmpty() @IsString() username: string;
-  @ApiProperty() @IsNotEmpty() @IsString() password: string;
-}
--- src/entities/common/user/dto/user-group.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UserGroupDTO {
-  @ApiProperty() titleTH: string;
-  @ApiProperty({ required: false }) titleEN: string;
-  @ApiProperty({ required: false }) titleCN: string;
-}
